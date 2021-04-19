175fb93bdf97d93ea9a6df519dcc36d2c8997409
Dario Leyva-Brown <1536300@mukilteo.wednet.edu>
2021-04-19 13:34:33 -0700

    Added water tile, began basic walk animation.

--- Assets/Dario/PlayerScript.cs
+++ Assets/Dario/PlayerScript.cs
@@ -6,13 +6,14 @@ public class PlayerScript : MonoBehaviour
 {
     public float MoveSpeed;
     private Rigidbody2D rb;
-
+    private Animator an;
+    bool IsWalking;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
+        an = GetComponent<Animator>();
 
     }
 
@@ -20,7 +21,17 @@ public class PlayerScript : MonoBehaviour
     void Update()
     {
         rb.velocity = new Vector2(Input.GetAxis("Horizontal") * MoveSpeed, Input.GetAxis("Vertical") * MoveSpeed);
+        if (rb.velocity != new Vector2(0, 0))
+        {
+            IsWalking = true;
+            an.SetBool("IsWalking", true);
+        }
+        else
+        {
+            IsWalking = false;
+            an.SetBool("IsWalking", false);
 
+        }
 
     }
 }
