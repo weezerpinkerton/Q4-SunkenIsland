ab7dbe521bd5232f4c3523d295966aaf15cf001c
Dario Leyva-Brown <1536300@mukilteo.wednet.edu>
2021-04-19 12:16:53 -0700

    Added Player and basic movement script.

---
+++ Assets/Dario/PlayerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float MoveSpeed;
+    private Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * MoveSpeed, Input.GetAxis("Vertical") * MoveSpeed);
+
+
+    }
+}
