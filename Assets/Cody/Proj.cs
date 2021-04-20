79e28420dede1a016ad7d9479788c0111d97ff46
Dario Leyva-Brown <dleyvabrown@gmail.com>
2021-04-20 12:39:44 -0700

    Cody enemy ai and projectiles

---
+++ Assets/Cody/Proj.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Proj : MonoBehaviour
+{
+    public float speed;
+    private Vector2 target;
+    private Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        target = new Vector2(player.position.x, player.position.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        if (transform.position.x == target.x && transform.position.y == target.y)
+        {
+            DestroyProjectile();
+
+        }
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision == true)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Cody/ShooterAI.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterAI : MonoBehaviour
+{
+    public float retreatdistance;
+    private float timeBettweenshots;
+    public float starttimeBettweenshots;
+    public float speedchasing;
+    public float speedRetreating;
+    public float Stoppingdistance;
+
+    public GameObject proj;
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, player.position) > Stoppingdistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speedchasing * Time.deltaTime);
+        }
+        else if (Vector2.Distance(transform.position, player.position) < Stoppingdistance && Vector2.Distance(transform.position, player.position) > retreatdistance)
+        {
+            transform.position = this.transform.position;
+        }
+
+        else if (Vector2.Distance(transform.position, player.position) < retreatdistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -speedRetreating * Time.deltaTime);
+        }
+
+
+        if (timeBettweenshots <= 0)
+        {
+            Instantiate(proj, transform.position, Quaternion.identity);
+            timeBettweenshots = starttimeBettweenshots;
+
+        }
+        else
+        {
+            timeBettweenshots -= Time.deltaTime;
+        }
+    }
+}
