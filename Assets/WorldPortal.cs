8401686ab901af5ea284ee36716779ad6ee958de
Dario Leyva-Brown <dleyvabrown@gmail.com>
2021-05-13 18:24:52 -0700

    Added tiles, made tutorial level with lighting, basic portal script,

---
+++ Assets/WorldPortal.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WorldPortal : MonoBehaviour
+{
+    public string NextScene = "Level1";
+    public Animator transition;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetKey("e"))
+        {
+            StartCoroutine(LoadLevel());
+        }
+    }
+
+
+    IEnumerator LoadLevel()
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(1);
+
+        SceneManager.LoadScene(NextScene);
+    }
+
+}
